0c011a928a0898c0fa0a93c1392fdbb29e4f977a
CaioRC <caiorcbr2@gmail.com>
2022-02-23 20:56:36 -0300

    Fix for vite React-Router problem

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
 
   return (
-    <Router >
+    <Router basename='/dolla_website' >
       <Routes >
         <Route path='/' element={<Home/>} />
         <Route path='/signin' element={<SignIn/>} />
